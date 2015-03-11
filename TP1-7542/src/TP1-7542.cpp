c34f2696d4299864878a9fbff7faa91fd93f741c
marcosRiarte <mriarte@fi.uba.ar>
2015-03-11 18:07:38 -0300

    Create TP1-7542.cpp

---
+++ TP1-7542/src/TP1-7542.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TP1-7542.cpp
+// Author      : Marcos Riarte
+// Version     :Prueba 1.0
+// Copyright   :
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
