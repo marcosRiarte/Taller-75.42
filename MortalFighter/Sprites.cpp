8448b60f2d706bf694f2a8e4464bac01b78ab91f
marcosRiarte <mriarte@fi.uba.ar>
2015-04-04 15:07:47 -0300

    Interfeces de la clase Sprites

---
+++ MortalFighter/Sprites.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "Sprites.h"
+
+
+Sprites::Sprites()
+{
+}
+void determinarClips()
+{
+}
+int seleccionarClips()
+{
+}
+void dibujarClip(){
+}
+
+Sprites::~Sprites()
+{
+}
---
+++ MortalFighter/Sprites.h
@@ -0,0 +1,12 @@
+#pragma once
+class Sprites
+{
+public:
+	Sprites();
+
+	void determinarClips();
+	int seleccionarClips();
+	void dibujarClip();//Va a la vista
+	~Sprites();
+};
+
