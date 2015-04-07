77b46438efe18deb33ce776aa23aae0a6782613f
KL44S <agustinbinci@hotmail.com>
2015-04-07 16:33:26 -0300

    Algunas modificaciones al controlador. Es un espanto

--- MortalFighter/Controlador.cpp
+++ MortalFighter/Controlador.cpp
@@ -19,22 +19,49 @@ MOV_TIPO Controlador::cambiar(){
 		case SDLK_ESCAPE:
 			return CERRAR;
 		case SDLK_UP:
+			if (state[SDL_SCANCODE_DOWN]) return QUIETO;
 			if ((state[SDL_SCANCODE_RIGHT]))
 				return SALTODER;
 			if ((state[SDL_SCANCODE_LEFT]))
 				return SALTOIZQ;
 			return ARRIBA;
 		case SDLK_DOWN:
+			//ESTA PORQUERIA HACE QUE NO SE MUEVA SI APRETAS DOS COSAS A LA VEZ. 
+			//ES UN ESPANTO Y ESTA MAL PERO NO LE ENCONTRE LA VUELTA PARA HACERLO DE OTRA FORMA.
+			if ((state[SDL_SCANCODE_LEFT]) || (state[SDL_SCANCODE_UP]) || (state[SDL_SCANCODE_RIGHT])) return QUIETO;
 			return ABAJO;
 		case SDLK_RIGHT:
+			if ((state[SDL_SCANCODE_LEFT]) || (state[SDL_SCANCODE_UP]) || (state[SDL_SCANCODE_DOWN])) return QUIETO;
 			return DER;
 		case SDLK_LEFT:
+			if ((state[SDL_SCANCODE_RIGHT]) || (state[SDL_SCANCODE_UP]) || (state[SDL_SCANCODE_DOWN])) return QUIETO;
 			return IZQ;
 		case SDL_QUIT:
 				return CERRAR;
-		default:
-			return QUIETO;  //Quieto a la izquierda
 		}
+	}
+
+	//TODA ESTA MIERDA QUE SIGUE ES LA QUE HACE QUE EL PERSONAJE SE MUEVA RAPIDO CUANDO DEJAS APRETADA ALGUNA TECLA.
+	//SI NO SIRVE SE COMENTA Y CHAU.
+	//PERO DE ESTA FORMA SI TENES PULSADA ALGUNA TECLA DESPUES DE SALTAR POR EJEMPLO SE SIGUE MOVIENDO.
+	if (state[SDL_SCANCODE_RIGHT]){
+		if ((state[SDL_SCANCODE_LEFT]) || (state[SDL_SCANCODE_UP]) || (state[SDL_SCANCODE_DOWN])) return QUIETO;
+		return DER;
+	}
+
+	if (state[SDL_SCANCODE_LEFT]){
+		if ((state[SDL_SCANCODE_RIGHT]) || (state[SDL_SCANCODE_UP]) || (state[SDL_SCANCODE_DOWN])) return QUIETO;
+		return IZQ;
+	}
+
+	if (state[SDL_SCANCODE_UP]){
+		if ((state[SDL_SCANCODE_RIGHT]) || (state[SDL_SCANCODE_LEFT]) || (state[SDL_SCANCODE_DOWN])) return QUIETO;
+		return ARRIBA;
+	}
+
+	if (state[SDL_SCANCODE_DOWN]){
+		if ((state[SDL_SCANCODE_RIGHT]) || (state[SDL_SCANCODE_LEFT]) || (state[SDL_SCANCODE_UP])) return QUIETO;
+		return ABAJO;
 	}
 
 	return QUIETO;
