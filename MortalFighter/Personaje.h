2d707c50624c775f23b2b209be478b9a4ba7043c
giss-ignacio <giss.ignacio@gmail.com>
2015-05-16 19:58:47 -0300

    Se saca codigo basura

--- MortalFighter/Personaje.h
+++ MortalFighter/Personaje.h
@@ -25,7 +25,7 @@ class Personaje
 {
 public:
 	std::string nombre;
-	Personaje::Personaje(float anchoPersonaje, float altoPersonaje, int zIndexPersonaje, std::string unaorientacion, std::string spritesDirPersonaje, std::string CaminarParaAdelante, std::string CaminarParaAtras, std::string QuietoPersonaje, std::string SaltoPersonaje, std::string SaltoDiagonalPersonaje, std::string CaidaPersonaje, std::string PatadaAltaPersonaje, std::string GolpeadoPersonaje, std::string AgachadoPersonaje, std::string nombrePersonaje);
+	Personaje::Personaje(float anchoPersonaje, float altoPersonaje, int zIndexPersonaje, std::string unaorientacion, std::string spritesDirPersonaje, std::string nombrePersonaje);
 	float getAncho() const;
 	float getAlto() const;
 	int getZIndex() ;
@@ -34,17 +34,8 @@ public:
 	std::string getOrientacion() const;
 	std::string getSpriteDir() const;	
 	Sprite* getSprite() const;
-	std::string getCaminarParaAdelante() const;
-	std::string getCaminarParaAtras() const;
-	std::string getQuieto() const;
-	std::string getSalto() const;
-	std::string getSaltoDiagonal() const;
-	std::string getCaida() const;
 	std::string getNombre() const;
-	
-	std::string getPatadaAlta() const;
-	std::string getAgacharse() const;
-	std::string getGolpeado() const;
+
 	std::pair<int, int> getPosicionPx() const;
 	std::pair<int, int> Personaje::getPosicionPx(int altoSprite) const;
 	void setPosicionUn(float x, float y);
@@ -61,16 +52,8 @@ public:
 	void setZIndex(int unZindex);
 	void setOrientacion(std::string unaOrientacion);
 	void setSprites(std::string unosSprites);
-	void setCaminarParaAdelante(std::string unCaminarParaAdelante);
-	void setCaminarParaAtras(std::string unCaminarParaAtras);
-	void setQuieto(std::string unQuieto);
-	void setSalto(std::string unSalto);
-	void setSaltoDiagonal(std::string unSaltoDiagonal);
-	void setCaida(std::string UnaCaida);
+	
 	void setNombre(std::string unNombre);
-	void setPatadaAlta(std::string unaPatadaAlta);
-	void setAgacharse(std::string unAgacharse);
-	void setGolpeado(std::string unGolpeado);
 	
 
 
@@ -82,17 +65,9 @@ private:
 	int zIndex;
 	int vida;	
 	std::string orientacion;
-	std::string spritesDir;
-	std::string caminaradelante;
-	std::string caminaratras;
-	std::string quieto;
-	std::string salto;
-	std::string saltodiagonal;
-	std::string caida;
+	std::string spritesDir;	
 	std::string nombreP;
-	std::string patadaalta;
-	std::string agachado;
-	std::string golpeado;
+	
 	Sprite* sprites;
 	std::pair<float, float> posicionUn;
 	float deltaX;	
